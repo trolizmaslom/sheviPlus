faa858d1195a5ec28ff6eb9d0933d76c29a0d769
nickolay Gotsliyk <trolizmaslom@gmail.com>
2016-11-10 14:11:10 +0200

    some works

--- src/js/develop/develop_1.js
+++ src/js/develop/develop_1.js
@@ -1,8 +1,21 @@
+function shovDropdown() {
+    $('.brand').click(function () {
+        $(this).toggleClass('active');
+        if($(this).hasClass('active')){
+            $('.brand').not($(this)).removeClass('active').next().stop().slideUp();
+            $(this).next().stop().slideDown();
+        }else{
+            $(this).next().stop().slideUp();
+        }
+
+    });
+}
 
 
 
 $(document).ready(function(){
-
+    shovDropdown();
+    $('.search-block select').styler();
 });
 
 $(window).load(function(){
