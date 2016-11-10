22ce64778071fea97994a116f2a1fc73b4cdf457
Radiks <radion91@gmail.com>
2016-11-10 17:49:52 +0200

    sadldas'jlikafsd

--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -1,3 +1,29 @@
+function mapInit(){
+
+    var maps = $('.contacts .item-map-wrap');
+
+    maps.each(function(i, Element) {
+        var myLatlng = new google.maps.LatLng(mapObj[i].cordX,mapObj[i].cordY);
+        var myOptions = {
+            zoom: 13,
+            center: myLatlng,
+            disableDefaultUI: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            zoomControlOptions: {
+                position: google.maps.ControlPosition.LEFT_BOTTOM
+            }
+        };
+        var map = new google.maps.Map(Element, myOptions);
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            icon: mapMarker
+        });
+
+    });
+}
+
+
 
 
 
@@ -6,7 +32,7 @@ $(document).ready(function(){
 });
 
 $(window).load(function(){
-
+    mapInit();
 });
 
 $(window).resize(function(){
