dbde1e5f16413e8fb4c68df841fd1da5e9098ae4
Radiks <radion91@gmail.com>
2016-11-11 09:51:19 +0200

    contacts done ]

--- src/js/develop/develop_4.js
+++ src/js/develop/develop_4.js
@@ -1,11 +1,21 @@
 function mapInit(){
 
     var maps = $('.contacts .item-map-wrap');
+    var grayStyles = [
+        {
+            featureType: "all",
+            stylers: [
+                { saturation: -90 },
+                { lightness: 0 }
+            ]
+        }
+    ];
 
     maps.each(function(i, Element) {
         var myLatlng = new google.maps.LatLng(mapObj[i].cordX,mapObj[i].cordY);
         var myOptions = {
             zoom: 13,
+            styles: grayStyles,
             center: myLatlng,
             disableDefaultUI: true,
             mapTypeId: google.maps.MapTypeId.ROADMAP,
