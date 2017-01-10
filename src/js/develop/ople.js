16c8ff8faf09304cde081bba9897b2b8f8be82d9
nickolay Gotsliyk <trolizmaslom@gmail.com>
2017-01-10 12:57:30 +0200

    xzcb

--- src/js/develop/ople.js
+++ src/js/develop/ople.js
@@ -1,3 +1,19 @@
 /**
  * Created by nickolaygotsliyk on 28.12.16.
  */
+function tabNav() {
+    $('.tab-but').click(function (e) {
+        e.preventDefault;
+        if(!$(this).hasClass('active')){
+            $('.tab-but').removeClass('active');
+            $('.one-tab').removeClass('active');
+            $(this).addClass('active');
+            var i = $(this).index();
+            $('.one-tab').eq(i).addClass('active');
+
+        }
+    });
+}
+$(document).ready(function(){
+   tabNav();
+});
