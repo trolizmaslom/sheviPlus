c4804e8e2ec9459037a4f9dbd43c08c9640b0e0f
Yulia <julia4422@gmail.com>
2017-01-11 14:23:30 +0200

    commit

--- src/js/develop/develop_2.js
+++ src/js/develop/develop_2.js
@@ -2,7 +2,14 @@
 
 
 $(document).ready(function(){
+	$('#player').click(function(){
+		$(this).next('.play').css('display', 'none');
+	});
 
+	$('.play').click(function(){
+		$('#player').playVideo():Void;
+		$(this).css('display', 'none');
+	});
 });
 
 $(window).load(function(){
