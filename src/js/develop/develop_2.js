951f7fa6379c1d1ebcfaaf1b1135429d08b6e2ce
Yulia <julia4422@gmail.com>
2017-01-11 14:48:13 +0200

    not done video

--- src/js/develop/develop_2.js
+++ src/js/develop/develop_2.js
@@ -1,15 +1,36 @@
+// // Inject YouTube API script
+// var tag = document.createElement('script');
+// tag.src = "//www.youtube.com/player_api";
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
+// var player;
 
+// function onYouTubePlayerAPIReady() {
+//   player = new YT.Player('player', {
+//     events: {
+//       'onReady': onPlayerReady
+//     }
+//   });
+// }
+
+// function onPlayerReady(event) {
+//   var playButton = $(".play");
+//   playButton.addEventListener("click", function() {
+//     player.playVideo();
+//   });
+// }
 
 $(document).ready(function(){
-	$('#player').click(function(){
-		$(this).next('.play').css('display', 'none');
-	});
+	// onYouTubePlayerAPIReady();
 
-	$('.play').click(function(){
-		$('#player').playVideo():Void;
-		$(this).css('display', 'none');
-	});
+	// $('#player').click(function(){
+	// 	$(this).next('.play').css('display', 'none');
+	// });
+
+	// $('.play').click(function(){
+	// 	$(this).css('display', 'none');
+	// });
 });
 
 $(window).load(function(){
