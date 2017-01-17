f4decb91bf5975a6ececbe3a9d04727e1cd2a43b
nickolay Gotsliyk <trolizmaslom@gmail.com>
2017-01-17 12:13:14 +0200

    ki

--- src/js/develop/ople.js
+++ src/js/develop/ople.js
@@ -42,10 +42,30 @@ function fancyboxServices() {
         padding: '0',
          'afterLoad': function(){
             this.content.find('h3').text(text);
-        },
+        }
     })
 }
-
+function fancyTop() {
+    $('.opel-page .dropdown a').fancybox({
+        'beforeLoad': function(){
+            text = $(this.element).find('span').text();
+        },
+        openEffect : 'fade',
+        closeEffect : 'fade',
+        autoSize:true,
+        width : 1030,
+        height : 835,
+        maxWidth : '100%',
+        wrapCSS:'wrap-marka',
+        'closeBtn' : true,
+        fitToView:true,
+        autoCenter: true,
+        padding:'0',
+        'afterLoad': function(){
+            this.content.find('.title span').text(text);
+        }
+    });
+}
 function mapInit2() {
     if ($('.contactses .maper').length > 0) {
         var maps = $('.contactses .maper');
@@ -95,9 +115,24 @@ function mapInit2() {
         });
     }
 }
-
+function goTo2(){
+    $('header .nav a').click(function(e){
+        e.preventDefault();
+        var href = $(this).attr('href');
+        var target = $(href).offset().top;
+        if($(this).attr('data-lock') != undefined){
+            var tt = $(this).attr('data-lock');
+            if (tt == 'srv'){$('.srv_click').click();}
+            if (tt == 'zap'){$('.zap_click').click();}
+            if (tt == 'raz'){$('.raz_click').click();}
+        }
+        $(scroller).animate({scrollTop:target},500);
+    });
+}
 $(document).ready(function(){
+    goTo2();
    tabNav();
+    fancyTop();
    fancyboxGallery();
     mapInit2();
     fancyboxServices();
--- src/js/validate_script.js
+++ src/js/validate_script.js
@@ -256,7 +256,8 @@ $(document).ready(function () {
     validate('.clarify-middle .clarify-form form', {submitFunction: validationCall});
     validate('.service-form', {submitFunction: validationCall});
     validate('.discount-popup-form>form', {submitFunction: validationCall});
-    validate('.marka-content form', {submitFunction: validationCall});
+    validate('#marka-auto-op form', {submitFunction: validationCall});
+    validate('#marka-auto-ch form', {submitFunction: validationCall});
      validate('.services-form', {submitFunction: validationCall});
     
     Maskedinput();
