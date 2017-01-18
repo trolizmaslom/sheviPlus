fe0d3d83f10f210937bafad239ceccfaa209345a
nickolay Gotsliyk <trolizmaslom@gmail.com>
2017-01-18 12:51:37 +0200

    ро

--- src/lendos.php
+++ src/lendos.php
@@ -13,21 +13,33 @@ if(isset($_POST['contact_tel'])) {
     $tel = substr(htmlspecialchars(trim($_POST['contact_tel'])), 0, 100);
     $mess .= '<b>Телефон:</b>' . $tel . '<br>';
 }
-if(isset($_POST['contact_email'])) {
+if(isset($_POST['brand'])) {
     $mail = substr(htmlspecialchars(trim($_POST['brand'])), 0, 100);
     $mess .= '<b>Марка:</b>' . $mail . '<br>';
 }
-if(isset($_POST['contact_email'])) {
+if(isset($_POST['model'])) {
     $mail = substr(htmlspecialchars(trim($_POST['model'])), 0, 100);
     $mess .= '<b>Модель:</b>' . $mail . '<br>';
 }
-if(isset($_POST['contact_email'])) {
+if(isset($_POST['year'])) {
     $mail = substr(htmlspecialchars(trim($_POST['year'])), 0, 100);
     $mess .= '<b>Год:</b>' . $mail . '<br>';
 }
-if(isset($_POST['contact_email'])) {
+if(isset($_POST['zap-name'])) {
     $mail = substr(htmlspecialchars(trim($_POST['zap-name'])), 0, 100);
     $mess .= '<b>название запчасти:</b>' . $mail . '<br>';
+}
+if(isset($_POST['diagnostika'])) {
+    $mail = substr(htmlspecialchars(trim($_POST['diagnostika'])), 0, 100);
+    $mess .= '<b>Диагностика:</b>' . $mail . '<br>';
+}
+if(isset($_POST['service'])) {
+    $mail = substr(htmlspecialchars(trim($_POST['service'])), 0, 100);
+    $mess .= '<b>Услуга клиента:</b>' . $mail . '<br>';
+}
+if(isset($_POST['service-text'])) {
+    $mail = substr(htmlspecialchars(trim($_POST['service-text'])), 0, 100);
+    $mess .= '<b>Сообщение клиента:</b>' . $mail . '<br>';
 }
 $mess .= '<hr>';
 // подключаем файл класса для отправки почты
